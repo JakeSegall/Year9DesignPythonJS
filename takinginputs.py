4b7a150613883309c707d1891f0f284fc3a4b4c9
Jake <Jake.segall@GCCYJQ67J1WK.local>
2019-09-11 10:47:36 -0400

    taking inputs

---
+++ takinginputs.py
@@ -0,0 +1,17 @@
+#Comments are made with a #
+#It is essential with comment code.
+#input
+#this program will take two integers
+#and multiply them
+#all inputs start as strings
+#to change the type you cast it. 
+name = input("please input your name: ")
+a = input("plese input first number: ")
+a = int(a) 
+b = input("please input second number: ")
+#process
+product = a + b
+
+#output
+print("Hi, "+name)
+print("the product of "+str(a)+ "and "+str(b)+" is"+str(product)+"." )
