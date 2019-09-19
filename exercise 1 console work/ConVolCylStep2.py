1f99ddc1303d9879754d5c3fadfb0b431586f10c
Jake <Jake.segall@GCCYJQ67J1WK.local>
2019-09-19 15:32:14 -0400

    step 2 done

--- exercise 1 console work/ConVolCylStep2.py
+++ exercise 1 console work/ConVolCylStep2.py
@@ -1,20 +1,29 @@
 import math
+print("\n\tThe volume of a Cylinder is:")
+print("\n\t\t\tV = \u03C0\u00d7radius\u00b2\u00d7height")
+print("\n\tThis program will take as input the radius and height")
+print("\tand print the volume.")
 
-Print("Step 2")
+print("Step 2")
 #Input
 #What inputs are needed to calculate the volume of a cylinder?
 print("Volume of a Cylinder Formula: ")
 
 name = input("What is your name: ")   #takes users name
 
-radius = input("Input radius(cm): ")  #input
-radius = (int)(radius)     #cast to int
+radius = input("\n\tInput radius(cm): ")  #input
+radius = (int)(radius)    #cast to int
 
 height = input("Input height(cm): ") #input
 height = (int)(height)     #cast to int
 #Process
 #What formula is used to calculate the volume of a cylinder?
-volume = 3.14*radius*radius*height
+volume = math.pi*radius*radius*height 
+volume = round(volume,2)
 #Output
 #What is important about the output?
-print("The volume is: "+str(volume))
+print("\n\tHi "+name+"!")
+print("Give a cylinder with:")
+print("Radius = "+str(radius))
+print("Height = "+str(height))
+print("The volume is: "+str(volume))  
